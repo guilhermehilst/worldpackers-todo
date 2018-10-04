fb7eefbc48ffddf4065685cdd020b00d86c4bc49
Guilherme Hilst Ribeiro <guilhermehilst@gmail.com>
2018-10-04 17:06:37 -0300

    Fix event column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_143800) do
+ActiveRecord::Schema.define(version: 2018_10_04_193210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
-    t.string "type"
+    t.string "event_type"
     t.json "payload"
     t.bigint "task_id"
     t.datetime "created_at", null: false
--- spec/models/events_spec.rb
+++ spec/models/events_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
 
-  it { should validate_presence_of(:type) }
+  it { should validate_presence_of(:event_type) }
   it { should validate_presence_of(:payload) }
   it { should belong_to(:task) }
 
