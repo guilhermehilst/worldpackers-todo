a68e491ade05155dab6069edc6c54b0dc7e59576
Guilherme Hilst Ribeiro <guilhermehilst@gmail.com>
2018-10-05 01:09:11 -0300

    Fix Event factory

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :event do
     task
     event_type { 'event type' }
+    completed_at { Time.zone.now - 10.minutes }
     payload { { colour: 'nice color', phrase: 'nice phrase' } }
   end
 end
